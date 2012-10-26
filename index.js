1d991567043c5c0416af31f981a89715dfcb6f4b
Nathan Aschbacher <nathan@coradine.com>
2012-10-26 14:40:49 -0700

    First version to get hammered on.

--- index.js
+++ index.js
@@ -24,39 +24,45 @@
 
 var fs = require('fs');
 
-var Chinood = function Chinood(models_array, client) {
-    models_array = models_array instanceof Array ? models_array : [models_array];
-
+var Chinood = function Chinood(models, client) {
     this.client = client;
-    this.models = {};
-    
-    for(var i = 0, length = models_array.length; i < length; i++) {
-        this.registerModel(models_array[i]);
-    }
+
+    this.models = models || {};
+    this.models[Chinood.BaseModel.name] = Chinood.BaseModel;
 };
 
 Chinood.BaseModel = require('./lib/base_model.js');
 
 Chinood.init = function(models_root, client) {
+    client.defaults.mime['application/json'].encode = Chinood.modelEncoder;
+
     models_root = models_root.charAt(models_root.length-1) == '/' ? models_root : models_root + '/';
         
     var files = fs.readdirSync(models_root);
-    var models = [Chinood.BaseModel];
+    var models = { BaseModel: Chinood.BaseModel };
 
     for(var i = 0, length = files.length; i < length; i++) {
         files[i] = models_root+files[i];
-        models.push(require(files[i]));
+        model = require(files[i]);
+        model.client = client;
+        models[model.name] = model;
     }
-
     return new Chinood(models, client);
 };
 
+Chinood.modelEncoder = function(data) {
+    function modelToKey(prop, value) {
+        if(value && prop !== '' && ((value).constructor == Chinood.BaseModel || (value).constructor.super_ == Chinood.BaseModel)) {
+            return value.key;
+        }
+        return value;
+    }
+    return JSON.stringify(data, modelToKey);
+};
+
 Chinood.prototype.registerModel = function(model) {
-    var client = this.client;
-    //this.models[model.name] = model.bind(this);
-    this.models[model.name] = function(init_data, check_constraints) {
-        return new model(init_data, check_constraints, client);
-    };
+    model.client = this.client;
+    this.models[model.name] = model;
 };
 
 module.exports = Chinood;
