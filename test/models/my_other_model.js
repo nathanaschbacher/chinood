d78420c566ac9a3003caa846716caeeaad74f020
Nathan Aschbacher <nathan.aschbacher@gmail.com>
2012-11-02 03:28:15 -0700

    Updated the MyOtherModel sample with new declaration syntax.

--- test/models/my_other_model.js
+++ test/models/my_other_model.js
@@ -23,15 +23,29 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 var chinood = require('../../index.js');
+var MyModel = require('./my_model.js');
 
 var MyOtherModel = function MyOtherModel(key, init_with, check_constraints) {
-    
-    this.attr('other_number', Number);
-    this.attr('other_string', String);
-    this.attr('other_array', Array);
-    this.attr('other_thing', Object);
+
+    this.attr('other_number', { is: Number, default: 5, index: true }); // automatically add value to secondary index.
+    this.attr('other_string', { default: "Hello There!"});
+    this.attr('other_array', { is: Array, of: Number }); // 'of' is a no-op, it's just for readability, there's no constraint enforced.
+    this.attr('other_thing', { is: Object, of: MyModel });
+    this.attr('other_function', { is: Function, default: get_other }); // this is a computed attribute, these values do not persist to Riak.
+    this.attr('other_date', { is: Date, default: new Date(), index: true });
 
     MyOtherModel.super_.apply(this, arguments);
 }; chinood.inherits(MyOtherModel, chinood.BaseModel);
 
+
+function get_other(_this) {
+    var temp = '';
+    for(var i = 0, times = _this.other_number; i < times; i++) {
+        temp += " " + _this.other_string + "\n\n";
+    }
+    return temp;
+}
+
 module.exports = MyOtherModel;
+
+
